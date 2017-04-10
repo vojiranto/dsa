f0a475928a021bb83fe86d6a3babaa888c3bb1d8
Al Lastor <al.lastor@bk.ru>
2017-04-10 11:51:04 +0300

    v0.1.0.0.2.1

--- src/Stratification.hs
+++ src/Stratification.hs
@@ -7,6 +7,11 @@ import Data.Matrix
 import Data
 import Graph
 
+
 toStratification :: Imagination -> Stratification
 toStratification (Imagination d c) = Stratification
     d 2 ((,[1,2]) <$> c)
+
+-- подразбиение на более мелкие уровни
+celling :: [Int] -> [Int]
+celling = concatMap $! \c -> [c*2, c*2-1]
