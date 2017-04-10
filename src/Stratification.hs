e0ddce19b1427b8b0513a866a2bd96807a450f89
Al Lastor <al.lastor@bk.ru>
2017-04-10 12:24:39 +0300

    v0.1.0.0.2.2

--- src/Stratification.hs
+++ src/Stratification.hs
@@ -12,6 +12,13 @@ toStratification :: Imagination -> Stratification
 toStratification (Imagination d c) = Stratification
     d 2 ((,[1,2]) <$> c)
 
+
 -- подразбиение на более мелкие уровни
 celling :: [Int] -> [Int]
 celling = concatMap $! \c -> [c*2, c*2-1]
+
+
+shiftCeil :: Diameter -> Diameter -> F Point -> (Ceil,[Int])
+    -> [(Ceil, [Int])]
+shiftCeil d1 d2 fp  = undefined
+
