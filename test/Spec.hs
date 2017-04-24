da26eacf759979a6f671b5a041750ee17a82fa52
Al Lastor <al.lastor@bk.ru>
2017-04-24 20:41:31 +0300

    v.0.1.0.0.6.7

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,9 @@
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.Gdk.GC
 import Graphics.UI.Gtk hiding (Color, Point, Object)
+import Control.Monad
+import Data.IORef
+import Point
 
 defaultFgColor :: Color
 defaultFgColor = Color 65535 65535 65535
@@ -8,30 +11,101 @@ defaultFgColor = Color 65535 65535 65535
 defaultBgColor :: Color
 defaultBgColor = Color 0 0 0
 
-renderScene d ev = do
+renderScene d p ev = do
     dw     <- widgetGetDrawWindow d
     (w, h) <- widgetGetSize d
     gc     <- gcNew dw
     let fg = color 0 0 0
     gcSetValues gc $ newGCValues { foreground = fg }
-    drawPoint dw gc (220, 220)
-    drawPoint dw gc (22, 22)
-    drawRectangle dw gc True 0 0 10 10
+    let tr (x, y) = (
+            round (x *        toEnum w) `div` 6 + w`div`2,
+            round (y * (-1) * toEnum h) `div` 6 + h`div`2)
+        drawLine' x y = drawLine dw gc (tr x) (tr y)
+
+    -- Оси OX и OY
+    drawLine' (0, -3) (0, 3)
+    drawLine' (-3, 0) (3, 0)
+
+    -- Насечки (мелкие)
+    forM_ [-2.9, -2.8..3] $ \i -> do
+        drawLine' (i, 0.01) (i, -0.01)
+        drawLine' (0.01, i) (-0.01, i)
+
+    -- насечки крупные
+    forM_ [-3..3] $ \i -> do
+        drawLine' (i, 0.1) (i, -0.1)
+        drawLine' (0.1, i) (-0.1, i)
+
+    drawPoints dw gc $ (\(Point x y) -> tr (x, y)) <$> p
     return True
 
 main :: IO ()
 main = do
     initGUI
     window  <- windowNew
+    hBox    <- hBoxNew False 4
+    vBox    <- vBoxNew False 4
+    hl@[h1, h2, h3,hx1,hx2,hy1, hy2, h4, h5] <- forM [1..9] $
+        const (hBoxNew False 4)
+    [   ar1, ar2, ar3,
+        arXmin, arXmax, arYmin, arYmax] <- forM [1..7] (const entryNew)
     drawing <- drawingAreaNew
-    windowSetTitle window "Cells"
-    containerAdd window drawing
+
+    set window [
+        windowDefaultWidth := 900,
+        windowDefaultHeight := 600,
+        containerChild := hBox,
+        windowTitle := "DSA – Dinamic Sistem Analiser"]
+
+    let setСontainers a b = do
+            set a $ (containerChild :=) <$> b
+
+
+    -- TODO проблема несовпадения типов vBox и drawing
+    setСontainers hBox [vBox]
+    setСontainers hBox [drawing]
+
+    l0 <- labelNewWithMnemonic "Введите формулы"
+    setСontainers vBox [l0]
+    boxSetChildPacking vBox l0 PackNatural 4 PackStart
+
+    setСontainers vBox hl
+
+    let extr hBox ar txt1 txt2 = do
+            lExt <- labelNewWithMnemonic txt1
+            setСontainers hBox [lExt]
+            setСontainers hBox [ar]
+            entrySetText  ar txt2
+
+    extr h1 ar1 "x' = " "1 + y - 1.4 * pot(x, 2)"
+    extr h2 ar2 "y' = " "0.3*x"
+
+    extr hx1 arXmin "x min = " "-1.5"
+    extr hx2 arXmax "x max = "  "1.5"
+    extr hy1 arYmin "y min = " "-1"
+    extr hy2 arYmax "y max = "  "1"
+    extr h3 ar3 "Число итераций = " "5"
+
+    [b1, b2] <- forM ["Образ", "Дополнительная итерация"]
+        buttonNewWithLabel
+
+    setСontainers h4 [b1, b2]
+
+    b3 <- buttonNewWithLabel "Спектр морса"
+    setСontainers h5 [b3]
+
+    -- Настраиваем упаковку аплетов
+    boxSetChildPacking hBox vBox PackNatural 4 PackStart
+    boxSetChildPacking hBox drawing PackGrow 4 PackStart
+
+    forM_ hl $ \a ->
+        boxSetChildPacking vBox a PackNatural 4 PackStart
+
     let bg = color 1 1 1
     widgetModifyBg drawing StateNormal bg
-    onExpose drawing (renderScene drawing)
+    onExpose drawing (renderScene drawing [])
 
     onDestroy window mainQuit
-    windowSetDefaultSize window 800 600
     windowSetPosition window WinPosCenter
     widgetShowAll window
     mainGUI
